e540a6013355b57eff99f15c2f745f37fb4dfb24
mingjingsi <30649118+mingjingsi@users.noreply.github.com>
2020-03-25 20:38:59 +0800

    Add files via upload

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -9,3 +9,11 @@ bLPM_Rcpp <- function(Pvalue, X, id, alpha, pi1_, pair_id, maxiter = 1e4L, tol =
     .Call('_LPM_bLPM_Rcpp', PACKAGE = 'LPM', Pvalue, X, id, alpha, pi1_, pair_id, maxiter, tol, coreNum)
 }
 
+single_LPM_noX_Rcpp <- function(Pvalue, alpha, pi1_, maxiter, tol) {
+    .Call('_LPM_single_LPM_noX_Rcpp', PACKAGE = 'LPM', Pvalue, alpha, pi1_, maxiter, tol)
+}
+
+single_LPM_Rcpp <- function(Pvalue, X, alpha, pi1_, maxiter = 1e4L, tol = 1e-6) {
+    .Call('_LPM_single_LPM_Rcpp', PACKAGE = 'LPM', Pvalue, X, alpha, pi1_, maxiter, tol)
+}
+
